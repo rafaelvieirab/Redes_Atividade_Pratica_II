1e5e6b9362d22329424dbe5466836bf3b071a7f9
rafaelvieirab <rafaelviebar@gmail.com>
2020-10-17 22:51:04 -0300

    feat: adicionando o form

--- Redes-Atividade-Pratica-II/src/br/ufc/crateus/server/Handle.java
+++ Redes-Atividade-Pratica-II/src/br/ufc/crateus/server/Handle.java
@@ -33,17 +33,22 @@ public class Handle implements  Runnable {
 		HttpResponse response = new HttpResponse();
 		response.addHeader("Date", new Date(System.currentTimeMillis()).toString());
 		response.addHeader("Connection", "Close");
+		response.addHeader("Content-Type", "text/html; charset=utf-8");
 		
 		if(!request.getVersion().equals("1.1")) {
 			response.setStatusCode(505);
 			response.setStatusText("HTTP Version Not Supported");
 			response.addHeader("Content-Length", "0");
 		}
+		else if(!request.getTarget().equals("/employees")) {
+			response.setStatusCode(404);
+			response.setStatusText("Not Found");
+			response.addHeader("Content-Length", "0");
+		}
 		else if(request.getMethod() == HttpMethod.GET) {
 			response.setStatusCode(200);
 			response.setStatusText("OK");
 			response.setBody(BuildHtmlResponse.toHtml(EmployeeDAO.getAllEmployees()));
-			response.addHeader("Content-Type", "text/html; charset=utf-8");
 			response.addHeader("Content-Length", String.valueOf(response.getBodyLength()));
 		}
 		else if(request.getMethod() == HttpMethod.POST) {
@@ -52,20 +57,17 @@ public class Handle implements  Runnable {
 				response.setStatusCode(201);
 				response.setStatusText("Created");
 				response.setBody(BuildHtmlResponse.toHtml(emp));
-				response.addHeader("Content-Type", "text/html; charset=utf-8");
 				response.addHeader("Content-Length", String.valueOf(response.getBodyLength()));
 			}
 			else {
 				response.setStatusCode(400);
 				response.setStatusText("Bad Request");
-				response.addHeader("Content-Type", "text/html; charset=utf-8");
 				response.addHeader("Content-Length", "0");
 			}
 		}
 		else {
 			response.setStatusCode(404);
 			response.setStatusText("Not Found");
-			response.addHeader("Content-Type", "text/html; charset=utf-8");
 			response.addHeader("Content-Length", "0");
 		}
 		
